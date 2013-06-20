e554497194a5ca3a0eeb2922294aeb508d0ad9c5
kooxiv <kooxiv@qq.com>
2013-06-20 11:55:16 +0800

    add Application & dbClient

--- src/com/kooxiv/klib/KDbClient.java
+++ src/com/kooxiv/klib/KDbClient.java
@@ -3,15 +3,26 @@
  */
 package com.kooxiv.klib;
 
+import garin.artemiy.sqlitesimple.library.SQLiteSimpleDAO;
+import android.content.Context;
+
 /**
  * 描述：
  * 
-<br/>
- * @author kooxiv@qq.com
-<br/>
- *  2013-6-20
-<br/>
+ * <br/>
+ * 
+ * @author kooxiv@qq.com <br/>
+ *         2013-6-20 <br/>
+ * @param <T>
  */
-public class KDbClient {
+public class KDbClient<T> extends SQLiteSimpleDAO<T> {
+
+	public KDbClient(Context context, Class<T> beanClass) {
+		super(beanClass, context);
+	}
+
+	public KDbClient(Context context, Class<T> beanClass, String dbName) {
+		super(beanClass, context, dbName);
+	}
 
 }
